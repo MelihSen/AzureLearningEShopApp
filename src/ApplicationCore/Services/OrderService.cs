5e213e10504fc93f37563bca29ec2b88047d553e
Melih Senyurt <melih_senyurt@epam.com>
2023-02-26 16:25:15 +0300

    Initial Commit. Azure service codes are located in </src/ApplicationCore/Services/OrderService.cs> file >> DeliveryOrderProc(); SendOrderItemsToServiceBus()

---
+++ src/ApplicationCore/Services/OrderService.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Ardalis.GuardClauses;
+using Microsoft.eShopWeb.ApplicationCore.Entities;
+using Microsoft.eShopWeb.ApplicationCore.Entities.BasketAggregate;
+using Microsoft.eShopWeb.ApplicationCore.Entities.OrderAggregate;
+using Microsoft.eShopWeb.ApplicationCore.Interfaces;
+using Microsoft.eShopWeb.ApplicationCore.Specifications;
+using System.Text;
+using Azure.Messaging.ServiceBus;
+
+namespace Microsoft.eShopWeb.ApplicationCore.Services;
+
+public class OrderService : IOrderService
+{
+    private readonly IRepository<Order> _orderRepository;
+    private readonly IUriComposer _uriComposer;
+    private readonly IRepository<Basket> _basketRepository;
+    private readonly IRepository<CatalogItem> _itemRepository;
+
+    public OrderService(IRepository<Basket> basketRepository,
+        IRepository<CatalogItem> itemRepository,
+        IRepository<Order> orderRepository,
+        IUriComposer uriComposer)
+    {
+        _orderRepository = orderRepository;
+        _uriComposer = uriComposer;
+        _basketRepository = basketRepository;
+        _itemRepository = itemRepository;
+    }
+
+    public async Task CreateOrderAsync(int basketId, Address shippingAddress)
+    {
+        var basketSpec = new BasketWithItemsSpecification(basketId);
+        var basket = await _basketRepository.FirstOrDefaultAsync(basketSpec);
+
+        Guard.Against.Null(basket, nameof(basket));
+        Guard.Against.EmptyBasketOnCheckout(basket.Items);
+
+        var catalogItemsSpecification = new CatalogItemsSpecification(basket.Items.Select(item => item.CatalogItemId).ToArray());
+        var catalogItems = await _itemRepository.ListAsync(catalogItemsSpecification);
+
+        var items = basket.Items.Select(basketItem =>
+        {
+            var catalogItem = catalogItems.First(c => c.Id == basketItem.CatalogItemId);
+            var itemOrdered = new CatalogItemOrdered(catalogItem.Id, catalogItem.Name, _uriComposer.ComposePicUri(catalogItem.PictureUri));
+            var orderItem = new OrderItem(itemOrdered, basketItem.UnitPrice, basketItem.Quantity);
+            return orderItem;
+        }).ToList();
+
+        var order = new Order(basket.BuyerId, shippingAddress, items);
+
+        await _orderRepository.AddAsync(order);
+
+        //delivery order processor
+        await DeliveryOrderProc(order);
+
+        //send to Service Bus
+        await SendOrderItemsToServiceBus(order);
+
+    }
+
+    public async Task DeliveryOrderProc(Order order)
+    {
+        var jsonData = JsonSerializer.Serialize(order);
+        ///api/OrderRequest
+        HttpClient client = new HttpClient() { BaseAddress = new Uri("<DeliveryOrderProc Azure Func Url>") };
+        client.DefaultRequestHeaders.Accept.Clear();
+        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+        StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+       
+        var result = await client.PostAsync("api/DeliveryOrderProc", stringContent);
+        
+        //if (result.IsSuccessStatusCode)
+        //    Console.WriteLine("Succesful");
+        //else
+        //    Console.WriteLine($"DeliveryOrderProc() failed. Error Code: {result.StatusCode}");
+    }
+
+    public async Task SendOrderItemsToServiceBus(Order order)
+    {
+        var orderedItems = order.OrderItems.Select(x => new OrderedItemForReserver {Id = x.ItemOrdered.CatalogItemId, UnitPrice = x.UnitPrice, Quantity = x.Units });
+
+        // connection string to your Service Bus namespace
+        string connectionString = "<Service Bus Connection String>";
+
+        // name of your Service Bus queue
+        string queueName = "ordereditemsqueue";
+
+        // the client that owns the connection and can be used to create senders and receivers
+        ServiceBusClient client;
+
+        // the sender used to publish messages to the queue
+        ServiceBusSender sender;
+
+        // Create the clients that we'll use for sending and processing messages.
+        client = new ServiceBusClient(connectionString);
+        sender = client.CreateSender(queueName);
+
+        // create a batch 
+        using ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
+
+        foreach (var item in orderedItems)
+        {
+            string data = JsonSerializer.Serialize(item);
+            messageBatch.TryAddMessage(new ServiceBusMessage(data));
+        }
+
+        try
+        {
+            // Use the producer client to send the batch of messages to the Service Bus queue
+            await sender.SendMessagesAsync(messageBatch);
+        }
+        finally
+        {
+            // Calling DisposeAsync on client types is required to ensure that network
+            // resources and other unmanaged objects are properly cleaned up.
+            await sender.DisposeAsync();
+            await client.DisposeAsync();
+        }
+    }
+}
